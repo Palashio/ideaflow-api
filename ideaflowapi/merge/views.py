4e0a4f87e08db8256ae28f4dda7b85e84d6f3b2d
Palash Shah <35114859+Palashio@users.noreply.github.com>
2021-11-10 20:14:36 -0500

    Update views.py

--- ideaflowapi/merge/views.py
+++ ideaflowapi/merge/views.py
@@ -11,7 +11,7 @@ import json
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= "app_creds.json"
 
 
-@api_view(['POST'])
+@api_view(['GET'])
 def merge_images(request):
     files = request.FILES.keys()
     #
