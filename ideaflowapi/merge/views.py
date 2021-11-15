ad799c31a57cc774b4bbfd9a72a59c5bb1e4a544
Palash Shah <35114859+Palashio@users.noreply.github.com>
2021-11-14 23:11:56 -0500

    updated changes

--- ideaflowapi/merge/views.py
+++ ideaflowapi/merge/views.py
@@ -7,6 +7,7 @@ from google.cloud import vision
 import cv2
 import io
 import json
+import numpy as np
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= "app_creds.json"
 
@@ -14,6 +15,7 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= "app_creds.json"
 @api_view(['POST'])
 def merge_images(request):
     files = request.FILES.keys()
+    data = request.data.keys()
     #
     if 'one' not in files or 'two' not in files:
         return JsonResponse({"MESSAGE": "BOTH IMAGES NOT PROVIDED"})
@@ -21,15 +23,20 @@ def merge_images(request):
     first_file, second_file = request.FILES['one'], request.FILES['two']
 
     IMG_NAME_1 = 'buffer/first_image.png'
-    IMG_NAME_2 = 'buffer/second_image.png'
+    # IMG_NAME_2 = 'buffer/second_image.png'
 
     default_storage.save(IMG_NAME_1, first_file)
-    default_storage.save(IMG_NAME_2, second_file)
+    default_storage.save(IMG_NAME_1, second_file)
 
     img1 = cv2.imread(IMG_NAME_1)
-    img2 = cv2.imread(IMG_NAME_2)
+    img2 = cv2.imread(IMG_NAME_1)
 
-    im_v = cv2.vconcat([img1, img1])
+    img1_rotation = get_rotation()
+
+    if 'vertical' in data:
+        im_v = cv2.hconcat([img1, img2])
+    else:
+        im_v = cv2.vconcat([img1, img2])
 
     client = vision.ImageAnnotatorClient()
 
@@ -46,8 +53,46 @@ def merge_images(request):
         OCR_RESULTS += str(text.description) + "\n"
 
     os.remove(IMG_NAME_1)
-    os.remove(IMG_NAME_2)
 
     return JsonResponse(json.dumps(OCR_RESULTS), safe=False, status=200)
 
+def process_image(incoming_image, rotation):
+    if rotation == 0:
+        return incoming_image
+    elif rotation == 90:
+        return cv2.rotate(incoming_image, cv2.cv2.ROTATE_90_CLOCKWISE)
+    elif rotation == 180:
+        return cv2.rotate(incoming_image, cv2.ROTATE_180)
+    else:
+        return cv2.rotate(incoming_image, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
+def get_rotation(response):
+    import pprint
+    texts = response.text_annotations
+
+    current = texts[0].bounding_poly.vertices
+    c_x, c_y = 0, 0
+
+    for i in range(4):
+        c_x += current[i].x
+        c_y += current[i].y
+
+    c_x = c_x / 4
+    c_y = c_y / 4
+
+    x_0 = current[0].x
+    y_0 = current[0].y
+
+
+    if x_0 < c_x:
+        if y_0 < c_y:
+            return 0
+        else:
+            return 270
+    else:
+        if y_0 < c_y:
+            return 90
+        else:
+            return 180
+
+        
